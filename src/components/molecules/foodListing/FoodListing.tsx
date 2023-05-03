86eecdc1a9ac0abc6c59f41f3edfd1a6d1acf23c
umar <umar.dev500@gmail.com>
2023-05-03 20:29:01 +0700

    redesign table

--- src/components/molecules/foodListing/FoodListing.tsx
+++ src/components/molecules/foodListing/FoodListing.tsx
@@ -1,3 +1,5 @@
+import { mdiFileEdit, mdiTrashCan } from '@mdi/js'
+import Icon from '@mdi/react'
 import React from 'react'
 
 interface Props {
@@ -11,11 +13,27 @@ export const FoodListing: React.FC<Props> = ({ num, name, price, thumb }) => {
     return (
         <tr className="">
             <td className="py-1.5 px-4 text-gray-600 text-center">{num}</td>
-            <td className="py-1.5 whitespace-nowrap px-4 text-gray-600">{name}</td>
-            <td className="py-1.5 px-4 ">
-                <img className="w-14 h-14 rounded-md" src={thumb} alt="thumb" />
+            <td className="py-1.5 whitespace-nowrap px-4 text-gray-600">
+                <div className="flex items-center gap-6">
+                    <img className="w-14 h-14 rounded-md" src={thumb} alt="thumb" />
+                    {name}
+                </div>
             </td>
+            {/* <td className="py-1.5 px-4 ">
+                <img className="w-14 h-14 rounded-md" src={thumb} alt="thumb" />
+            </td> */}
+            <td className="py-1.5 whitespace-nowrap px-4 text-gray-600 font-medium">Sep 23, 2023 15:15</td>
             <td className="py-1.5 whitespace-nowrap px-4 text-gray-600 font-medium ubuntu">IDR {price}</td>
+            <td className="py-1.5 whitespace-nowrap px-4">
+                <div className="flex gap-1">
+                    <button className="bg-red-700 hover:bg-red-800 px-2 py-1.5 rounded-md text-white">
+                        <Icon path={mdiTrashCan} size={'22px'} />
+                    </button>
+                    <button className="bg-sky-600 hover:bg-sky-700 px-2 py-1.5 rounded-md text-white">
+                        <Icon path={mdiFileEdit} size={'22px'} />
+                    </button>
+                </div>
+            </td>
         </tr>
     )
 }
--- src/components/organisms/foodList/FoodList.tsx
+++ src/components/organisms/foodList/FoodList.tsx
@@ -9,12 +9,18 @@ export const FoodList: React.FC = () => {
                     <tr className="text-gray-600 bg-gray-100">
                         <th className="py-4 px-4 w-20 font-medium text-center">#</th>
                         <th className="py-4 px-4 font-medium text-left">Name</th>
-                        <th className="py-4 px-4 font-medium text-left">Photo</th>
+                        {/* <th className="py-4 px-4 font-medium text-left">Photo</th> */}
+                        <th className="py-4 px-4 xl:w-64 font-medium text-left">Creatd Time</th>
                         <th className="py-4 px-4 xl:w-64 font-medium text-left">Price</th>
+                        <th className="py-4 px-4 xl:w-64 font-medium text-left">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     <FoodListing num={1} name="Mobile Legend Balance" price="24.000" thumb="assets/thumbs/MobileLegend-icon.jpg" />
+                    <FoodListing num={2} name="PUBG Mobile Diamond" price="54.000" thumb="assets/thumbs/PUBGM-icon.jpg" />
+                    <FoodListing num={3} name="Call of Duty Health" price="84.000" thumb="assets/thumbs/COD-icon.jpg" />
+                    <FoodListing num={4} name="AOV Membership" price="70.000" thumb="assets/thumbs/AOV-icon.jpg" />
+                    <FoodListing num={5} name="Point Blank Skin" price="280.000" thumb="assets/thumbs/PB-icon.jpg" />
                 </tbody>
             </table>
         </div>
