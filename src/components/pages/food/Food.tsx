a87dfc575c4084f0101df8e9d4a7c9689580de53
umar <umar.dev500@gmail.com>
2023-05-03 20:15:01 +0700

    repadding

--- src/components/pages/food/Food.tsx
+++ src/components/pages/food/Food.tsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 export const Food: React.FC = () => {
     return (
         <div className="container m-auto mt-10 p-6 rounded-md shadow-sm shadow-gray-300 bg-white">
-            <Link to="/" className="mt-1 inline-flex bg-sky-600 hover:bg-sky-700 text-white font-medium px-4 py-2.5 rounded-md">
+            <Link to="/" className="mt-1 inline-flex bg-sky-600 hover:bg-sky-700 text-white font-medium px-4 py-2 rounded-md">
                 Add new food
             </Link>
             <div>
