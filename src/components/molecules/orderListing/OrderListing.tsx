280db426b53927b974826d11fae2d0aed2752c58
umar <umar.dev500@gmail.com>
2023-05-03 19:15:48 +0700

    make order listing consisten

--- src/components/molecules/orderListing/OrderListing.tsx
+++ src/components/molecules/orderListing/OrderListing.tsx
@@ -11,7 +11,7 @@ export const OrderListing: React.FC<Props> = ({ title, times, price, image }) =>
     return (
         <div className="flex flex-wrap items-center cursor-pointer gap-4 border-b pb-4">
             <img className="w-20 h-16 rounded-md" src={image} alt="thumbs" />
-            <div className="flex flex-wrap gap-0.5 flex-1 justify-between">
+            <div className="flex flex-col xl:flex-row flex-wrap gap-0.5 flex-1 justify-between">
                 <h3 className="text-gray-600 font-medium">{title}</h3>
                 <div className="flex items-center gap-6">
                     <span className="text-gray-500 text-sm">X{times}</span>
