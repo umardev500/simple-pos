8ac279e666fcb599655437a729f48a81690236dc
umar <umar.dev500@gmail.com>
2023-05-03 19:46:09 +0700

    dynamic menu active class

--- src/components/organisms/menuList/MenuList.tsx
+++ src/components/organisms/menuList/MenuList.tsx
@@ -1,13 +1,17 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useMatch } from 'react-router-dom'
 
 export const MenuList: React.FC = () => {
+    const activeClasses = 'text-sky-600 border-sky-600'
+    const isRoot = useMatch('/') !== null
+    const isFoods = useMatch('/foods') !== null
+
     return (
         <nav className="flex items-stretch gap-6">
-            <Link to="/foods" className="px-4 flex items-center text-gray-500 font-medium hover:text-gray-600">
+            <Link to="/foods" className={`${isFoods ? activeClasses : 'border-b-white'} px-4 flex items-center text-gray-500 font-medium border-b-2 hover:text-gray-600`}>
                 Food
             </Link>
-            <Link to="/" className="px-4 flex items-center text-sky-600 font-medium border-b-2 border-sky-600">
+            <Link to="/" className={`${isRoot ? activeClasses : 'border-b-white'} px-4 flex items-center text-gray-500 font-medium border-b-2`}>
                 Transaction
             </Link>
         </nav>
