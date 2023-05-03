afec50d330977155b4f46a0ddb1a15340fa2f82b
umar <umar.dev500@gmail.com>
2023-05-03 19:06:10 +0700

    add shadow on hover on card

--- src/components/molecules/cardListing/CardListing.tsx
+++ src/components/molecules/cardListing/CardListing.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const CardListing: React.FC = () => {
     return (
-        <div className="bg-white shadow-xl overflow-clip rounded-md">
+        <div className="bg-white shadow-sm hover:shadow-lg shadow-gray-200 overflow-clip rounded-md cursor-pointer">
             <img src="assets/thumbs/AOV-icon.jpg" alt="thumb" />
             <div className="px-4 py-7 flex flex-col gap-0.5 items-center">
                 <h3 className="font-medium text-base text-gray-600 roboto">Arena of Valor Game</h3>
