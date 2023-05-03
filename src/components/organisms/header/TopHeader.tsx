0811b16a24239b7ff422755cad25666f9d498a54
umar <umar.dev500@gmail.com>
2023-05-03 18:52:10 +0700

    change bg color of top header

--- src/components/organisms/header/TopHeader.tsx
+++ src/components/organisms/header/TopHeader.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export const TopHeader: React.FC = () => {
     return (
-        <div className="bg-sky-500 px-4 h-16 flex items-start">
+        <div className="bg-white px-4 h-16 flex items-start border-b">
             <div className="container flex items-center gap-4 m-auto cursor-pointer">
                 <img className="w-11 h-11" src="assets/icons/salad.png" alt="" />
-                <h3 className="font-semibold text-white">Umar Resto</h3>
+                <h3 className="font-semibold text-gray-600">Umar Resto</h3>
             </div>
         </div>
     )
