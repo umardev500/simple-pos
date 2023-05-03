aed819dacc96ff00844a0b8bad5d08e099bdeb3d
umar <umar.dev500@gmail.com>
2023-05-03 19:37:11 +0700

    use Link component in MenuList

--- src/components/organisms/menuList/MenuList.tsx
+++ src/components/organisms/menuList/MenuList.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export const MenuList: React.FC = () => {
     return (
         <nav className="flex items-stretch gap-6">
-            <a href="#" className="px-4 flex items-center text-gray-500 font-medium hover:text-gray-600">
+            <Link to="/foods" className="px-4 flex items-center text-gray-500 font-medium hover:text-gray-600">
                 Food
-            </a>
-            <a href="#" className="px-4 flex items-center text-sky-600 font-medium border-b-2 border-sky-600">
+            </Link>
+            <Link to="/" className="px-4 flex items-center text-sky-600 font-medium border-b-2 border-sky-600">
                 Transaction
-            </a>
+            </Link>
         </nav>
     )
 }
