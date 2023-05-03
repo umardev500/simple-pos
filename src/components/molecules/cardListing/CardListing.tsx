e85d109b114430b4a87a8a0477cce362d2b8c569
umar <umar.dev500@gmail.com>
2023-05-03 19:11:17 +0700

    add zoom effect

--- src/components/molecules/cardListing/CardListing.tsx
+++ src/components/molecules/cardListing/CardListing.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 export const CardListing: React.FC = () => {
     return (
-        <div className="bg-white shadow-sm hover:shadow-lg shadow-gray-200 overflow-clip rounded-md cursor-pointer">
-            <img src="assets/thumbs/AOV-icon.jpg" alt="thumb" />
+        <div className="bg-white card shadow-sm hover:shadow-lg shadow-gray-200 overflow-clip rounded-md cursor-pointer">
+            <div className="overflow-clip img">
+                <img src="assets/thumbs/AOV-icon.jpg" alt="thumb" />
+            </div>
             <div className="px-4 py-7 flex flex-col gap-0.5 items-center">
                 <h3 className="font-medium text-base text-gray-600 roboto">Arena of Valor Game</h3>
                 <div className="text-gray-500 text-lg font-semibold ubuntu">IDR 50.000</div>
